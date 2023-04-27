e323e9074772330e002d72d3652614bfa528219d
MashaOvs <mashulia_chernecova@mail.ru>
2023-04-27 11:30:07 +0300

    lvl_2 finished

---
+++ Homeworks/lvl_2/task_2.3.py
@@ -0,0 +1,24 @@
+# Задача 2.3.
+
+# Напишите функцию, которая принимает цифры от 0 до 9 и возвращает значение прописью.
+# Например,
+# switch_it_up(1) -> 'One'
+# switch_it_up(3) -> 'Three'
+# switch_it_up(10000) -> None
+# Использовать условный оператор if-elif-else нельзя!
+
+def switch_it_up(number):
+    numbers = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    }
+    return numbers.get(number)
+number = int(input('Введите число от 1 до 9:'))
+print(switch_it_up(number))
---
+++ Homeworks/lvl_2/task_2.4.py
@@ -0,0 +1,48 @@
+# Задача 2.4.
+
+# Пункт A.
+# Напишите функцию, которая удаляет все восклицательные знаки из заданной строк.
+# Например,
+# foo("Hi! Hello!") -> "Hi Hello"
+# foo("") -> ""
+# foo("Oh, no!!!") -> "Oh, no"
+
+def remove_exclamation_marks(s):
+    s = s.replace('!','')
+    return s
+
+s = "Oh, no!!!"
+print(remove_exclamation_marks(s))
+        
+
+# Пункт B.
+# Удалите восклицательный знак из конца строки. 
+# remove("Hi!") == "Hi"
+# remove("Hi!!!") == "Hi!!"
+# remove("!Hi") == "!Hi"
+
+def remove_last_em(s):
+    if s[-1] == '!':
+       s = s[-1] + s[:-1]
+    return s
+
+s = "Hi!!!"
+print(remove_last_em(s))
+
+
+# Дополнительно
+
+# Пункт С.
+# Удалите слова из предложения, если они содержат ровно один восклицательный знак.
+# Слова разделены одним пробелом.
+# Например,
+# remove("Hi!") === ""
+# remove("Hi! Hi!") === ""
+# remove("Hi! Hi! Hi!") === ""
+# remove("Hi Hi! Hi!") === "Hi"
+# remove("Hi! !Hi Hi!") === ""
+# remove("Hi! Hi!! Hi!") === "Hi!!"
+# remove("Hi! !Hi! Hi!") === "!Hi!"
+
+def remove_word_with_one_em(s):
+    pass
